83d0a54d6f75caa145243fd33884acda40759116
d-car <danielscarstensen@gmail.com>
2018-08-25 01:14:04 -0500

    routes and base html shells working

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friends = [
     {
       name: "Johnny Bravo",
-      profilePic: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiLgr3-oYLdAhVM94MKHVJrDAkQjRx6BAgBEAU&url=https%3A%2F%2Fwww.buzzfeed.com%2Fkirstenking%2Fthats-all-folks&psig=AOvVaw38nNLDoChT9Alqth3Zmrlv&ust=1535082454195879",
+      profilePic: "https://images-na.ssl-images-amazon.com/images/I/41OgxVYuO8L._SX425_.jpg",
       scores: ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"]
     }
   ]
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,4 +5,9 @@ module.exports = function(app){
   app.get('/survey', function (req, res) {
     res.sendFile(path.join(__dirname + '/../public/survey.html'));
   });
+
+  // USE route returns home.html for any undefined GET routes.
+  app.use(function (req, res) {
+    res.sendFile(path.join(__dirname + '/../public/home.html'));
+  });
 };
